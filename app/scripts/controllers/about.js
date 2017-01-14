ecfcb4bbd72f06e9fb536c0fd79ea60bfb772ba4
Hubert Theodore <hubert@yup.com>
2017-01-14 16:11:55 +0900

    First push

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cosmosApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the cosmosApp
+ */
+angular.module('cosmosApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
